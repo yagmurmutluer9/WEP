3d1d644c87f5c74dfdb9020417f781f85e954edd
yagmurmutluer9 <yagmurmutluer9@gmail.com>
2021-05-19 14:40:30 +0300

    change email is done.

--- app/src/main/java/com/elif/wep/Profile.java
+++ app/src/main/java/com/elif/wep/Profile.java
@@ -3,8 +3,13 @@ package com.elif.wep;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -15,10 +20,20 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.internal.NavigationMenu;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Profile extends AppCompatActivity {
 
@@ -33,23 +48,36 @@ public class Profile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
 
         fAuth = FirebaseAuth.getInstance();
 
-        drawerNav();
-        navigationMenu();
+
 
         // change menu if user logged-in
 
         if (fAuth.getCurrentUser() != null) {
+            setContentView(R.layout.activity_profile);
+            drawerNav();
+            navigationMenu();
             navigation.getMenu().clear();
             navigation.inflateMenu(R.menu.menu_user); //inflate new items.
+
+            Button updateBtn = findViewById(R.id.updateBtn);
+            updateBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    getUserAuth();
+                }
+            });
         }
 
         if(fAuth.getCurrentUser() == null) {
+            setContentView(R.layout.activity_profile_logout);
+            drawerNav();
+            navigationMenu();
             navigation.getMenu().clear();
             navigation.inflateMenu(R.menu.menu); //inflate new items.
+
         }
 
 
@@ -171,6 +199,76 @@ public class Profile extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void getUserAuth() {
+
+        // --- dialog -- //
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Profile.this);
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+
+        View view = layoutInflater.inflate(R.layout.edit_profile_user_login, null);
+        alertDialog.setView(view);
+
+        AlertDialog userDialog = alertDialog.create();
+
+        final EditText email = view.findViewById(R.id.userEmail);
+        final EditText password = view.findViewById(R.id.userPassword);
+
+        // --- dialog -- //
+
+
+        EditText editUserEmail = findViewById(R.id.editUserMail);
+        String email_change = editUserEmail.getText().toString();
+
+
+        Button login = view.findViewById(R.id.loginAgain);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email_m = email.getText().toString().trim();
+                String password_m = password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email_change)) {
+                    Toast.makeText(Profile.this, "Please, enter password.", Toast.LENGTH_SHORT).show();
+                }
+
+                if (TextUtils.isEmpty(email_m) || TextUtils.isEmpty(password_m)) {
+                    Toast.makeText(Profile.this, "Enter every details", Toast.LENGTH_SHORT).show();
+                } else {
+                    fAuth.signInWithEmailAndPassword(email_m, password_m)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        FirebaseUser user = fAuth.getCurrentUser();
+                                        user.updateEmail(email_change)
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            Toast.makeText(Profile.this, "Successful.", Toast.LENGTH_SHORT).show();
+
+                                                        }
+                                                    }
+                                                });                                    } else {
+                                        Toast.makeText(Profile.this, "Please enter correct details", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+                }
+
+                userDialog.dismiss();
+            }
+
+        });
+
+        userDialog.show();
+
+
     }
 
 
